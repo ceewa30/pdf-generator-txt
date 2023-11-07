0d80a57aa6385797d359c6b83612b1510240597e
ceewa30 <ceewa30@gmail.com>
2023-11-07 11:42:02 -0500

    Finished version

---
+++ README.md
@@ -0,0 +1,19 @@
+pip install fpdf
+
+pip install pandas
+
+ # setting up version control git
+
+    git config --global user.name "ceewa30"
+    git config --global user.email "ceewa30@gmail.com"
+    git config --global push.default matching
+    git config --global alias.co checkout
+    git init
+
+    git add .
+    git commit -am 'Inital commit'
+
+    or push an existing repository from the command line
+git remote add origin https://github.com/ceewa30/Django-CRM.git
+git branch -M main
+git push -u origin main - or - git push -f -u origin main  
---
+++ main.py
@@ -0,0 +1,25 @@
+from fpdf import FPDF
+import pandas as pd
+
+pdf = FPDF(orientation="P", unit="mm", format="A4")
+pdf.set_auto_page_break(auto=False, margin=0)
+
+df = pd.read_csv("topics.csv", sep=",")
+
+for index, row in df.iterrows():
+    for i in range(row["Pages"]):
+        pdf.add_page()
+        pdf.set_font(family="Times", style="B", size=24)
+        pdf.set_text_color(100, 100, 100)
+        pdf.cell(w=0, h=12, txt=row["Topic"], align="L", ln=1)
+        for j in range(20, 290, 8):
+            pdf.line(10, j, 200, j)
+
+        pdf.ln(264)
+
+        pdf.set_font(family="Times", style="I", size=8)
+        pdf.set_text_color(180, 180, 180)
+        pdf.cell(w=0, h=12, txt=row["Topic"], align="R", ln=1)
+
+
+pdf.output("output.pdf")
